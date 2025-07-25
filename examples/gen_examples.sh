fd09d8544e71f51922ac1a8207645f4dc3b98030
Adrien GIVRY <contact@adrien-givry.com>
2025-07-25 15:30:32 -0400

    Moving away from premake5 to CMake everywhere!

--- examples/gen_examples.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-version=${1:-gmake}
-
-pushd "$(dirname "$0")" > /dev/null
-premake5 "$version" --compile-vulkan --compile-mock --gfxapi=vulkan
-popd > /dev/null
---
+++ gen_proj_linux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pushd "$(dirname "$0")" > /dev/null
+cmake -S . -B build/ -DORHI_COMPILE_VULKAN=ON -DORHI_COMPILE_MOCK=ON -DORHI_GENERATE_EXAMPLES=ON
+popd > /dev/null
