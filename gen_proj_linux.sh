f2430201ba5c1a6dbb95d2cf28358454b5cd8e7e
Adrien GIVRY <contact@adrien-givry.com>
2025-07-29 22:36:28 -0400

    Fixed project generation scritps

--- gen_proj_linux.sh
+++ gen_proj_linux.sh
@@ -2,7 +2,7 @@
 
 pushd "$(dirname "$0")" > /dev/null
 cmake -S . -B build \
-    -DORHI_EXAMPLES_GFX_API=vulkan \
+    -DORHI_DEFAULT_GFX_API=vulkan \
     -DORHI_COMPILE_VULKAN=ON \
     -DORHI_COMPILE_MOCK=ON \
     -DORHI_GENERATE_EXAMPLES=ON
