0b9460a572ab1ccdcb474626a8ee15e03fd012d8
Adrien GIVRY <contact@adrien-givry.com>
2025-07-29 21:37:20 -0400

    Updated deps config

--- gen_proj_win32.bat
+++ gen_proj_win32.bat
@@ -1,5 +1,7 @@
 @echo off
 
 pushd "%~dp0"
-cmake -S . -B build -DORHI_COMPILE_VULKAN=ON -DORHI_COMPILE_MOCK=ON -DORHI_GENERATE_EXAMPLES=ON
+cmake -S . -B build^
+    -DORHI_COMPILE_VULKAN=ON^
+    -DORHI_COMPILE_MOCK=ON
 popd
