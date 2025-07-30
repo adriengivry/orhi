b92205a29e3cbd126942438a96a67346c0cf0612
Adrien GIVRY <contact@adrien-givry.com>
2025-07-29 21:44:04 -0400

    Fixed linux config and fixed wrong cmake option values

--- gen_proj_linux.sh
+++ gen_proj_linux.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 pushd "$(dirname "$0")" > /dev/null
-cmake -S . -B build/ -DORHI_COMPILE_VULKAN=ON -DORHI_COMPILE_MOCK=ON -DORHI_GENERATE_EXAMPLES=ON
+cmake -S . -B build \
+    -DORHI_COMPILE_VULKAN=ON \
+    -DORHI_COMPILE_MOCK=ON \
+    -DORHI_GENERATE_EXAMPLES=ON
 popd > /dev/null
--- gen_proj_win32.bat
+++ gen_proj_win32.bat
@@ -3,5 +3,6 @@
 pushd "%~dp0"
 cmake -S . -B build^
     -DORHI_COMPILE_VULKAN=ON^
-    -DORHI_COMPILE_MOCK=ON
+    -DORHI_COMPILE_MOCK=ON^
+    -DORHI_GENERATE_EXAMPLES=ON
 popd
